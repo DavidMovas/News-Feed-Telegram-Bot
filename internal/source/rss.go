284839bde59e1d25c0a5f2103b088ceba2f01ab0
Nofre <davidprogram417@gmail.com>
2024-10-01 22:09:02 +0200

    added rss logic

--- internal/source/rss.go
+++ internal/source/rss.go
@@ -1,6 +1,12 @@
 package source
 
-import "telbot/internal/model"
+import (
+	"context"
+	"telbot/internal/model"
+
+	"github.com/SlyMarbo/rss"
+	"github.com/samber/lo"
+)
 
 type RSSSource struct {
 	URL        string
@@ -13,5 +19,49 @@ func NewRSSSourceFromModel(m model.Source) RSSSource {
 		URL:        m.FeedURL,
 		SourceID:   m.ID,
 		SourceName: m.Name,
+	}
+}
+
+func (r RSSSource) Fetch(ctx context.Context) ([]model.Item, error) {
+	feed, err := r.loadFeed(ctx, r.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	return lo.Map(feed.Items, func(item *rss.Item, _ int) model.Item {
+		return model.Item{
+			Title:      item.Title,
+			Categories: item.Categories,
+			Link:       item.Link,
+			Date:       item.Date,
+			Summary:    item.Summary,
+			SourceName: r.SourceName,
+		}
+	}), nil
+}
+
+func (r RSSSource) loadFeed(ctx context.Context, url string) (*rss.Feed, error) {
+	var (
+		feedCh = make(chan *rss.Feed)
+		errCh  = make(chan error)
+	)
+
+	go func() {
+		feed, err := rss.Fetch(url)
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		feedCh <- feed
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case err := <-errCh:
+		return nil, err
+	case feed := <-feedCh:
+		return feed, nil
 	}
 }

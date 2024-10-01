99f1d8a184bc1fb206dbeb9773a26d1b600fbdac
Nofre <davidprogram417@gmail.com>
2024-10-01 21:52:30 +0200

    added goose migrations and models

---
+++ internal/source/rss.go
@@ -0,0 +1,17 @@
+package source
+
+import "telbot/internal/model"
+
+type RSSSource struct {
+	URL        string
+	SourceID   int64
+	SourceName string
+}
+
+func NewRSSSourceFromModel(m model.Source) RSSSource {
+	return RSSSource{
+		URL:        m.FeedURL,
+		SourceID:   m.ID,
+		SourceName: m.Name,
+	}
+}
